4d42cce5b35f0b261a5bc1a1bd2dabcf1bc5fa40
DiandraKejoraPutri <156975662+DiandraKejoraPutri@users.noreply.github.com>
2024-12-08 21:54:23 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\android_2024\\perpus_crud\\perpus" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=D:\\android_2024\\perpus_crud\\perpus"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\android_2024\\perpus_crud\\perpus\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\android_2024\\perpus_crud\\perpus"
+  "FLUTTER_TARGET=D:\\android_2024\\perpus_crud\\perpus\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\android_2024\\perpus_crud\\perpus\\.dart_tool\\package_config.json"
+)
